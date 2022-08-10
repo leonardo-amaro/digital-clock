bb4d942d6f1c63d4e2865956983bf4fc7bf7597d
Leonardo <leonardo.amarosilva01@gmail.com>
2022-08-09 21:51:02 -0300

    Update script.js file

--- script.js
+++ script.js
@@ -1,11 +1,18 @@
+function doubleNum(num) {
+    return num < 10 ? "0" + num : num
+};
+
 const clock = document.querySelector("#clock-display");
 
 const update = setInterval(() => {
-    var date = new Date();
+    let date = new Date();
+    let hours = date.getHours();
+    let minutes = date.getMinutes();
+    let seconds = date.getSeconds();
     
     clock.innerHTML = `
         <p>
-            ${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}
+            ${doubleNum(hours)}:${doubleNum(minutes)}:${doubleNum(seconds)}
         </p>
     `;
 }, 1000/15);
